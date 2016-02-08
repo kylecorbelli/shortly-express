be77ec161b7f0f405ce70bba37635240c648cc4e
pacificLiving <kyle.corbelli@gmail.com>
2016-02-08 14:36:07 -0800

    Add users table to database

--- app/config.js
+++ app/config.js
@@ -39,5 +39,21 @@ db.knex.schema.hasTable('clicks').then(function(exists) {
 // Add additional schema definitions below
 /************************************************************/
 
+db.knex.schema.hasTable('users').then(function(exists) {
+  if (!exists) {
+    db.knex.schema.createTable('users', function (user) {
+      user.increments('id').primary();
+      user.string('username', 255);
+      user.string('password', 100);
+      user.string('salt', 100);
+      user.timestamps();
+    }).then(function (table) {
+      console.log('Created Table', table);
+    });
+  }
+});
+
+
+
 
 module.exports = db;
--- app/models/user.js
+++ app/models/user.js
@@ -3,8 +3,16 @@ var bcrypt = require('bcrypt-nodejs');
 var Promise = require('bluebird');
 
 
+var Link = require('./link.js');
+
+
 
 var User = db.Model.extend({
+  tableName: 'users',
+  hasTimestamps: true,
+  links: function() {
+    return this.hasMany(Link);
+  }
 });
 
 module.exports = User;
